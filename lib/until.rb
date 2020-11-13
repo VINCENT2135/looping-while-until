09a85655faa39e33bc4cc372ae1b1505457c03cd
Vincent Ivos <vincentivos1@gmail.com>
2020-11-13 13:18:21 -0500

    commit tree b615cd874841a5702217e4f7fa980892fc629e22

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
-end
+    levitation_force = 6
+    #your code here
+    count = 10
+    until levitation_force == count 
+      puts "Wingardium Leviosa"
+      levitation_force += 1
+  end
+  end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,10 @@
 def using_while
   levitation_force = 6
-	
+  count = 10
+  while levitation_force < count
+   puts "Wingardium Leviosa"
+   levitation_force += 1
   #your code here
 end
-
+end
 
